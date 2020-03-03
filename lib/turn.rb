33810909cb27d003b6be5a60839e6efe873b8435
Adam Spencer <aspencer0919@gmail.com>
2020-03-03 21:28:40 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,49 @@
+board = [" "," ", " ", " ", " ", " ", " ", " ", " "]
+user_input = ""
+player_character = "X"
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  converted_input = user_input.to_i - 1
+end
+
+def valid_move?(board, index)
+  def position_taken?(array, ind)
+    if array[ind] == " " || array[ind] == "" || array[ind] == nil
+      return false
+    else
+      return true
+    end
+  end
+  def on_board?(num)
+    if num.between?(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
+    return true
+  else
+    return false
+  end
+end
+
+def move(board, converted_input, player_character="X")
+  board[converted_input] = player_character
+end
+
+def turn (user_input)
+  puts "Please enter 1-9:"
+  user_input = gets.chomp
+  input_to_index (user_input)
+  valid_move? (user_input)
+end
